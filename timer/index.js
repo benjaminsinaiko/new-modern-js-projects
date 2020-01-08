c30097fab742b3d31ae6a0a796abb400f9269ca5
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-08 17:32:05 -0600

    Add start and pause methods to Timer

--- timer/index.js
+++ timer/index.js
@@ -5,11 +5,21 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
-  start() {
-    console.log('Time to start the timer!');
-  }
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    console.log('tick');
+  };
 }
 
 const durationInput = document.querySelector('#duration');
