d4a025f86536f369c9005e7119584b0d25296d38
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-09 13:45:10 -0600

    Set up results dropdown widget

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -13,7 +13,20 @@ const fetchData = async searchTerm => {
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input type="text" class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
