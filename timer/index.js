b58663864c516128993f419a86d19d3805485067
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-08 23:05:50 -0600

    Initial timer callback setup for start, tick, and complete

--- timer/index.js
+++ timer/index.js
@@ -1,14 +1,22 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
@@ -20,8 +28,10 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      this.onComplete();
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      this.onTick();
     }
   };
 
@@ -38,4 +48,14 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer started');
+  },
+  onTick() {
+    console.log('Timer just clicked down');
+  },
+  onComplete() {
+    console.log('Timer completed');
+  }
+});
