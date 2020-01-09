212ac60652acd76eef5e367a209f92e84fb12c2a
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-09 11:41:50 -0600

    Set up movie title search with axios

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: keys.OMDB_KEY,
+      s: searchTerm
+    }
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
