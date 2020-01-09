8a2d59a863a705133758920bff73c2ef43131d6e
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-09 12:27:23 -0600

    Render movies from search results

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -6,13 +6,29 @@ const fetchData = async searchTerm => {
     }
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = event => {
-  fetchData(event.target.value);
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
+
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title} (${movie.Year})</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
+  }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
