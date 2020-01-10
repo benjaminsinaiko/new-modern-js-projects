fd782e21ce1f9b9fe2f4f75f4a3c30185be04d74
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-10 11:18:46 -0600

    Refactor - extract selection logic

--- movie-fight/autocomplete.js
+++ movie-fight/autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root, renderOption }) => {
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) => {
   root.innerHTML = `
   <label><b>Search For a Movie</b></label>
   <input type="text" class="input" />
@@ -31,8 +31,8 @@ const createAutoComplete = ({ root, renderOption }) => {
       option.innerHTML = renderOption(movie);
       option.addEventListener('click', () => {
         dropdown.classList.remove('is-active');
-        input.value = movie.Title;
-        onMovieSelect(movie);
+        input.value = inputValue(movie);
+        onOptionSelect(movie);
       });
 
       resultsWrapper.appendChild(option);
--- movie-fight/index.js
+++ movie-fight/index.js
@@ -24,6 +24,12 @@ createAutoComplete({
       <img src="${imgSrc}" />
       ${movie.Title} (${movie.Year})
     `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
   }
 });
 
