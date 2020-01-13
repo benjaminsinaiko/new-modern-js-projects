e91d43c33ad0ff0c59fa200c42a04d06d4cbd284
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-13 10:12:41 -0600

    Add random shapes, remove wireframes mode

--- maze/index.js
+++ maze/index.js
@@ -1,13 +1,17 @@
 const { Engine, Render, Runner, World, Bodies, Mouse, MouseConstraint } = Matter;
 
+const WIDTH = 800;
+const HEIGHT = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    WIDTH: 800,
+    HEIGHT: 600,
+    wireframes: false
   }
 });
 Render.run(render);
@@ -37,4 +41,11 @@ const walls = [
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes
+for (let i = 0; i < 35; i++) {
+  if (Math.random() > 0.5) {
+    World.add(world, Bodies.rectangle(Math.random() * WIDTH, Math.random() * HEIGHT, 50, 50));
+  } else {
+    World.add(world, Bodies.circle(Math.random() * WIDTH, Math.random() * HEIGHT, 35));
+  }
+}
