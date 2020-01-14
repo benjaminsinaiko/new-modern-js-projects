16dfc7163dce68c24e11f9b9ae5f3b027649097c
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-14 12:39:00 -0600

    Generate link url from encrypted message

--- message/index.js
+++ message/index.js
@@ -4,5 +4,23 @@ document.querySelector('form').addEventListener('submit', event => {
   const input = document.querySelector('#message-input');
   const encrypted = btoa(input.value);
 
-  document.querySelector('#link-input').value = encrypted;
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
+
+document.querySelector('.copy').addEventListener('click', () => {
+  const linkURL = document.querySelector('#link-input');
+  copyText(linkURL);
+});
+
+function copyText(textToCopy) {
+  textToCopy.select();
+  document.execCommand('copy');
+
+  const copiedMessage = document.querySelector('.copied');
+  copiedMessage.classList.remove('hidden');
+  setTimeout(() => {
+    copiedMessage.classList.add('hidden');
+  }, 500);
+}
