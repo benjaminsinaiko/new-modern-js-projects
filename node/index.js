c14f4f27430fae22267965e1828b29c50d78b7ea
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-15 13:15:46 -0600

    Add promise solutions to isFile

--- node/index.js
+++ node/index.js
@@ -1,33 +1,37 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// Method #1
+// const lstat = filename => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(stats);
+//     });
+//   });
+// };
+
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  const allStats = Array(filenames.length).fill(null);
-
   for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
-
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-
-      allStats[index] = stats;
-
-      const ready = allStats.every(stats => {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
+    try {
+      const stats = await lstat(filename);
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
