ef4a91168a483d97abbf7e6d3acbdb8f733aba3d
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-15 12:56:42 -0600

    Add callback solution to isFile in nls program

--- node/index.js
+++ node/index.js
@@ -7,5 +7,27 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.log(err);
   }
 
-  console.log(filenames);
+  const allStats = Array(filenames.length).fill(null);
+
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      allStats[index] = stats;
+
+      const ready = allStats.every(stats => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
