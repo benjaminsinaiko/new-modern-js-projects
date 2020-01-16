b773967cfd78e81756d3df85f49bc27fd2d8a419
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-16 11:40:52 -0600

    Add node child process to watchit

--- watchit/index.js
+++ watchit/index.js
@@ -3,6 +3,8 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
 
 program
   .version('0.0.1')
@@ -17,7 +19,7 @@ program
     }
 
     const start = debounce(() => {
-      console.log('STARTING USERS PROGRAM');
+      spawn('node', [name], { stdio: 'inherit' });
     }, 100);
 
     chokidar
