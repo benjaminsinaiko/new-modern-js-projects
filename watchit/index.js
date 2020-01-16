fd259b4197e7f9b4d59274d7e2221687c6b44554
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-16 12:03:09 -0600

    Kill old process add notifications

--- watchit/index.js
+++ watchit/index.js
@@ -5,6 +5,7 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program
   .version('0.0.1')
@@ -18,8 +19,13 @@ program
       throw new Error(`Could not find the file <${name}>`);
     }
 
+    let proc;
     const start = debounce(() => {
-      spawn('node', [name], { stdio: 'inherit' });
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.blue('>>>> Starting process...'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
     }, 100);
 
     chokidar
---
+++ watchit/test.js
@@ -0,0 +1,1 @@
+console.log('watchit is working!');
