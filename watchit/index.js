8c153602dc6514913db13207a09470888c60b1b6
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-16 11:22:13 -0600

    Add and setup chokidar, lodash.debounce, caporal

--- watchit/index.js
+++ watchit/index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-console.log('I was executed');
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(args => {
+    console.log(args);
+  });
+
+program.parse(process.argv);
+
+const start = debounce(() => {
+  console.log('STARTING USERS PROGRAM');
+}, 100);
+
+chokidar
+  .watch('.')
+  .on('add', start)
+  .on('change', () => console.log('FILE CHANGED'))
+  .on('unlink', () => console.log('FILE UNLINKED'));
