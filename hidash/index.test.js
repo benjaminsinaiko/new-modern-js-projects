a82cc285e00aea37fe38eff30c8f9264163fb17e
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-27 11:57:14 -0600

    Refactor test file

--- hidash/index.test.js
+++ hidash/index.test.js
@@ -1,31 +1,38 @@
 const { forEach, map } = require('./index');
 
-// ####################
-// TEST: forEach
-// ####################
-let sum = 0;
-forEach([1, 2, 3], value => {
-  sum += value;
+const test = (desc, fn) => {
+  console.log('---', desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+test('The forEach function', () => {
+  let sum = 0;
+  forEach([1, 2, 3], value => {
+    sum += value;
+  });
+
+  if (sum !== 6) {
+    throw new Error('Expected summing array to equal 6');
+  }
 });
 
-if (sum !== 6) {
-  throw new Error('Expected summing array to equal 6');
-}
+test('The map function', () => {
+  const result = map([1, 2, 3], value => {
+    return value * 2;
+  });
 
-// ####################
-// TEST: map
-// ####################
-const result = map([1, 2, 3], value => {
-  return value * 2;
+  if (result[0] !== 2) {
+    throw new Error(`Expected 2, but found ${result[0]}`);
+  }
+
+  if (result[1] !== 4) {
+    throw new Error(`Expected 4, but found ${result[1]}`);
+  }
+  if (result[2] !== 6) {
+    throw new Error(`Expected 6, but found ${result[2]}`);
+  }
 });
-
-if (result[0] !== 2) {
-  throw new Error(`Expected 2, but found ${result[0]}`);
-}
-
-if (result[1] !== 4) {
-  throw new Error(`Expected 4, but found ${result[1]}`);
-}
-if (result[2] !== 6) {
-  throw new Error(`Expected 6, but found ${result[2]}`);
-}
