011fab8a1a86a6609f23885d85f5271f5fd85b67
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-27 16:40:12 -0600

    Add to testing file, helper functions and assertions

--- movie-fight/tests/autocomplete.test.js
+++ movie-fight/tests/autocomplete.test.js
@@ -1,4 +1,23 @@
-it('Shows an autocomplete', () => {
+const waitFor = selector => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 2000);
+  });
+};
+
+beforeEach(() => {
+  document.querySelector('#target').innerHTML = '';
+
   createAutoComplete({
     root: document.querySelector('#target'),
     fetchData() {
@@ -8,8 +27,32 @@ it('Shows an autocomplete', () => {
       return movie.Title;
     }
   });
+});
+
+it('Dropdown starts closed', () => {
+  const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).not.to.include('is-active');
+});
+
+it('After searching, dropdown opens up', async () => {
+  const input = document.querySelector('input');
+  input.value = 'avengers';
+  input.dispatchEvent(new Event('input'));
+
+  await waitFor('.dropdown-item');
 
   const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).to.include('is-active');
+});
 
-  expect(dropdown.className).not.to.include('is-active');
+it('After searching, displays some results', async () => {
+  const input = document.querySelector('input');
+  input.value = 'avengers';
+  input.dispatchEvent(new Event('input'));
+
+  await waitFor('.dropdown-item');
+
+  const items = document.querySelectorAll('.dropdown-item');
+
+  expect(items.length).to.equal(3);
 });
