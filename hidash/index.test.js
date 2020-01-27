e169e0189b5ecfa05112907471db4eac9badfcc7
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-27 12:24:47 -0600

    Refactor code to work with mocha

--- hidash/index.test.js
+++ hidash/index.test.js
@@ -1,16 +1,7 @@
 const assert = require('assert');
 const { forEach, map } = require('./index');
 
-const test = (desc, fn) => {
-  console.log('---', desc);
-  try {
-    fn();
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-test('The forEach function', () => {
+it('The forEach function', () => {
   let sum = 0;
   forEach([1, 2, 3], value => {
     sum += value;
@@ -19,14 +10,10 @@ test('The forEach function', () => {
   assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
 
-test('The map function', () => {
+it('The map function', () => {
   const result = map([1, 2, 3], value => {
     return value * 2;
   });
-
-  // assert.strictEqual(result[0], 2);
-  // assert.strictEqual(result[1], 4);
-  // assert.strictEqual(result[2], 6);
 
   assert.deepStrictEqual(result, [2, 4, 6]);
 });
